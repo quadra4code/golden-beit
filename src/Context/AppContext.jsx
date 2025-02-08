37d0a5b8a7e3fcba32acf8993ddcc34eaa73903b
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2025-02-08 06:10:45 +0200

    push

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -4,14 +4,9 @@ const AppContext = createContext();
 export const AppProvider = ({children}) => {
   const [popupContent, setPopupContent] = useState('')
   const [popupHeader, setPopupHeader] = useState('')
-  const [testPass, setTestPass] = useState('')
   const [isOpen, setIsOpen] = useState(false);
-  const [authDone, setAuthDone] = useState(false);
   const [singleUnit, setSingleUnit] = useState();
   const [filterData, setFilterData] = useState();
-  const shareScreenPass = '3126531265#';
-  const videoRef = useRef(null); // Reference to the video element
-  const [isSharing, setIsSharing] = useState(false); // State to track screen sharing
   console.log(popupContent, popupHeader);
   const token = localStorage.getItem('token')
   useEffect(()=>{
@@ -25,45 +20,11 @@ export const AppProvider = ({children}) => {
     })
     .catch(error => console.error(error))
   },[])
-  const startSharing = async()=>{
-    try {
-      // Request screen capture
-      const stream = await navigator.mediaDevices.getDisplayMedia({
-        video: true,
-        audio: false, // Set to true if you want to share audio as well
-      });
-      // Set the stream to the video element
-      if (videoRef.current) {
-        videoRef.current.srcObject = stream;
-      }
-      setIsSharing(true);
-      // Handle when the user stops sharing
-      stream.getTracks().forEach((track) => {
-        track.onended = () => {
-          setIsSharing(false);
-        };
-      });
-    } catch (error) {
-      console.error('Error sharing screen:', error);
-    }
-  }
-  const handleCheckPass = ()=> {
-    if(testPass===shareScreenPass){
-      setIsOpen(false)
-      setAuthDone(true)
-      console.log(authDone);
-      startSharing()
-    }else{
-      setIsOpen(false)
-      setAuthDone(false)
-    }
-  }
   return (
     <AppContext.Provider 
       value={{isOpen, setIsOpen, popupContent,
       setPopupContent, popupHeader, setPopupHeader ,setSingleUnit,singleUnit,
-      filterData, setFilterData ,shareScreenPass,startSharing,
-      setTestPass, testPass, handleCheckPass,setAuthDone
+      filterData, setFilterData 
       }}>
       {children}
     </AppContext.Provider>
--- src/Pages/TestScreenSharing.jsx
+++ src/Pages/TestScreenSharing.jsx
@@ -3,19 +3,45 @@ import AppContext from '../Context/AppContext';
 import Popup from '../Components/Popup';
 
 const ScreenShare = () => {
-  const {setIsOpen, authDone} = useContext(AppContext)
+  const {setIsOpen, authDone, setTestPass} = useContext(AppContext)
   const videoRef = useRef(null); // Reference to the video element
   const [isSharing, setIsSharing] = useState(false); // State to track screen sharing
+  const [inpPass, setInpPass] = useState()
   const startScreenShare = async () => {
-    if (!authDone) return setIsOpen(true);
+    try {
+      if(inpPass!=='3126531265#'){
+        return
+      }
+      // Request screen capture
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: false, // Set to true if you want to share audio as well
+      });
+      // Set the stream to the video element
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+      setIsSharing(true);
+      // Handle when the user stops sharing
+      stream.getTracks().forEach((track) => {
+        track.onended = () => {
+          setIsSharing(false);
+        };
+      });
+    } catch (error) {
+      console.error('Error sharing screen:', error);
+    }
   };
   return (
     <>
       <Popup/>
       <div className='share-screen-container'>
-        <button onClick={startScreenShare} disabled={isSharing}>
-          {isSharing ? 'Sharing...' : 'بدء عرض الشاشة'}
-        </button>
+        <div className='header'>
+          <button onClick={startScreenShare} disabled={isSharing}>
+            {isSharing ? 'Sharing...' : 'بدء عرض الشاشة'}
+          </button>
+          <input onChange={(e)=>{setInpPass(e.target.value)}} type='password' className='pass' />
+        </div>
         <video ref={videoRef} autoPlay playsInline muted style={{ width: '100%' }} />
         <iframe width="100%" height="709" src="https://www.youtube.com/embed/pc-dAFilXYk" title="كيف تحولت من تارك للصلاة إلى متلذذ بالصلاة؟" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
