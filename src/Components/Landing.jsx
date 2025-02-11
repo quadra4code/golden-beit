bedadf61a62bf5b0b68b5ae4148636c2413aabf5
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2025-02-11 17:02:48 +0200

    bg

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -4,10 +4,12 @@ import SearchBar from './SearchBar';
 const Landing = () => {
   return (
     <section className='landing'>
-      <div className='hero'>
-        <h1>استكشف منزل أحلامك</h1>
-        <h4>نقدم لعملائنا خيارات سكنية مبتكرة ومتنوعة، مدعومة بأحدث التقنيات وأدوات البحث لتلبية جميع الميزانيات والتفضيلات</h4>
-        <Link to=''>ابدأ الآن</Link>
+      <div className="opacity">
+        <div className='hero'>
+          <h1>استكشف منزل أحلامك</h1>
+          <h4>نقدم لعملائنا خيارات سكنية مبتكرة ومتنوعة، مدعومة بأحدث التقنيات وأدوات البحث لتلبية جميع الميزانيات والتفضيلات</h4>
+          <Link to=''>ابدأ الآن</Link>
+        </div>
       </div>
       <SearchBar/>
     </section>
