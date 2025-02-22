14062c69cdd008cb66d82202284ee74168709dad
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2025-02-23 01:22:43 +0200

    push

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -26,64 +26,66 @@ const Footer = () => {
           <img src={logo} alt="logo" />
           <span>نحن نقربك من منزل أحلامك<br/> بنقرة واحدة في كل مرة</span>
         </div>
-        <ul>
-          <li className='header'>ماذا عنا</li>
-          <li>
-            <HashLink 
-              scroll={(el) => {
-                const yOffset = -60; // Adjust this value to match the height of your navbar
-                const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
-                window.scrollTo({ top: y, behavior: 'smooth' });
-              }}
-              smooth to='/#why-us'>
-                من نحن
-            </HashLink>
-          </li>
-          <li>
-            <HashLink 
-              scroll={(el) => {
-                const yOffset = -60; // Adjust this value to match the height of your navbar
-                const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
-                window.scrollTo({ top: y, behavior: 'smooth' });
-              }}
-              smooth to='/#why-us'>
-                الإستشارات
-            </HashLink>
-          </li>
-          <li><Link to='/contact-us'>تواصل معنا</Link></li>
-        </ul>
-        <ul>
-          <li className='header'>الروابط</li>
-          <li><Link to='add-new-unit'>أضف وحدتك</Link></li>
-          <li><Link to='/all-units'>جميع الوحدات</Link></li>
-          <li><Link to='inquiry-page'>استعلام عن الفائزين</Link></li>
-          <li><Link to='/loader-board'>لوحة المتصدرين</Link></li>
-        </ul>
-        <ul>
-          <li className='header'>Find Us</li>
-          <li>Events</li>
-          <li>Locations</li>
-          <li>Newsletter</li>
-        </ul>
-        {/* <ul>
-          <li className='header'>Our Social</li>
-          <li>
-            <a href="https://www.instagram.com/golden_beit/">Instagram</a>
-            <FaInstagram/>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/golden.beit/">Facebook</a>
-            <AiOutlineFacebook/> 
-          </li>
-          <li>
-            <a href="https://www.tiktok.com/@golden_beit">TikTok</a>
-            <RiTiktokFill /> 
-          </li>
-          <li>
-            <a href="https://www.youtube.com/@Golden_Beit">Youtube</a>
-            <RiYoutubeFill />
-          </li>
-        </ul> */}
+        <div className='links'>
+          <ul>
+            <li className='header'>ماذا عنا</li>
+            <li>
+              <HashLink 
+                scroll={(el) => {
+                  const yOffset = -60; // Adjust this value to match the height of your navbar
+                  const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                  window.scrollTo({ top: y, behavior: 'smooth' });
+                }}
+                smooth to='/#why-us'>
+                  من نحن
+              </HashLink>
+            </li>
+            <li>
+              <HashLink 
+                scroll={(el) => {
+                  const yOffset = -60; // Adjust this value to match the height of your navbar
+                  const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                  window.scrollTo({ top: y, behavior: 'smooth' });
+                }}
+                smooth to='/#why-us'>
+                  الإستشارات
+              </HashLink>
+            </li>
+            <li><Link to='/contact-us'>تواصل معنا</Link></li>
+          </ul>
+          <ul>
+            <li className='header'>الروابط</li>
+            <li><Link to='add-new-unit'>أضف وحدتك</Link></li>
+            <li><Link to='/all-units'>جميع الوحدات</Link></li>
+            <li><Link to='inquiry-page'>استعلام عن الفائزين</Link></li>
+            <li><Link to='/loader-board'>لوحة المتصدرين</Link></li>
+          </ul>
+          <ul>
+            <li className='header'>Find Us</li>
+            <li>Events</li>
+            <li>Locations</li>
+            <li>Newsletter</li>
+          </ul>
+          {/* <ul>
+            <li className='header'>Our Social</li>
+            <li>
+              <a href="https://www.instagram.com/golden_beit/">Instagram</a>
+              <FaInstagram/>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/golden.beit/">Facebook</a>
+              <AiOutlineFacebook/> 
+            </li>
+            <li>
+              <a href="https://www.tiktok.com/@golden_beit">TikTok</a>
+              <RiTiktokFill /> 
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@Golden_Beit">Youtube</a>
+              <RiYoutubeFill />
+            </li>
+          </ul> */}
+        </div>
       </section>
     </footer>
   )
