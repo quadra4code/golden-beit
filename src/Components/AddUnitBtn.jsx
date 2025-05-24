df3b3aee567685923ab9d36a4d4e8e7c9fa8f29c
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2025-05-24 22:01:53 +0300

    Update AddUnitBtn.jsx
    edit link to all units

--- src/Components/AddUnitBtn.jsx
+++ src/Components/AddUnitBtn.jsx
@@ -3,7 +3,7 @@ const AddUnitBtn = () => {
   return (
     <div className="add-new-unit-btn-wrapper">
       <div className="add-new-unit-btn">
-        <a href="/add-new-unit" className="btn">
+        <a href="/all-units" className="btn">
           <span>عرض جميع الوحدات</span>
         </a>
         <IoMdArrowBack />
